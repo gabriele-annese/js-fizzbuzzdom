fc14ab20ee0b09da2125c0b2be59dbb6a0532dc7
gabriele-annese <gannese06@gmail.com>
2021-10-25 15:38:05 +0200

    add comment

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
 
 const container = document.querySelector(".container");
 // scrivere ciclo da 1 a 100
-// gen 10 row 
 
 for(let square = 1; square <= 100; square++){
     let rowMarkup = '<div class="row">';
     rowMarkup += `<div class="square">${square}</div>`
+    // multipli-3
     if(square % 3 ==0 ){
         rowMarkup = `<div class="square ml3">fizz</div>`
     }
+    // multipli-5
     if(square % 5 ==0 ){
         rowMarkup = `<div class="square ml5">buzz</div>`
     }
-
+    // multipli 3-5
     if( (square % 3 ==0 ) && (square % 5 ==0 ) ){
         rowMarkup = `<div class="square ml">fizz-buzz</div>`
     }
 
-
+    // print in html
     rowMarkup += '</div>'
     container.innerHTML += rowMarkup
 }
