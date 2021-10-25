30fb7e7d757ad343b021b878e3b0ef6d0003bdc1
gabriele-annese <gannese06@gmail.com>
2021-10-25 13:48:48 +0200

    add ciclo 1 a 100

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+
+const container = document.querySelector(".container");
+
 // scrivere ciclo da 1 a 100
+// gen 10 row 
+
+for (let square = 1; square <= 100; square++) {
+let rowMarkup = '<div class="row">';
+rowMarkup += `<div class="square">${square}</div>`;
+rowMarkup += '</div>';
+container.innerHTML += rowMarkup;
+}
+
+// individuare multipli di 3 
+// individuare multipli di 5
+// individuare multipli di 3 e 5 
