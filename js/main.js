b50a5df5f5d398fb844be0aed2e050bde97331c9
gabriele-annese <gannese06@gmail.com>
2021-10-25 15:33:24 +0200

    add multipli 3-5

--- js/main.js
+++ js/main.js
@@ -1,28 +1,24 @@
 
 const container = document.querySelector(".container");
-
 // scrivere ciclo da 1 a 100
 // gen 10 row 
 
-for (let square = 1; square <= 100; square++) {
+for(let square = 1; square <= 100; square++){
     let rowMarkup = '<div class="row">';
-    if( square % 3 ==  0){
-        rowMarkup += `<div class="square ml3">fizz</div>`;
-        rowMarkup += '</div>';
-        container.innerHTML += rowMarkup;
-    } else if (square % 5 == 0){
-        rowMarkup += `<div class="square ml5">buzz</div>`;
-        rowMarkup += '</div>';
-        container.innerHTML += rowMarkup;
-    } else{
-        let rowMarkup = '<div class="row">';
-        rowMarkup += `<div class="square">${square}</div>`;
-        rowMarkup += '</div>';
-        container.innerHTML += rowMarkup;
+    rowMarkup += `<div class="square">${square}</div>`
+    if(square % 3 ==0 ){
+        rowMarkup = `<div class="square ml3">fizz</div>`
+    }
+    if(square % 5 ==0 ){
+        rowMarkup = `<div class="square ml5">buzz</div>`
     }
 
-}
+    if( (square % 3 ==0 ) && (square % 5 ==0 ) ){
+        rowMarkup = `<div class="square ml">fizz-buzz</div>`
+    }
 
-// individuare multipli di 3 
-// individuare multipli di 5
+
+    rowMarkup += '</div>'
+    container.innerHTML += rowMarkup
+}
 // individuare multipli di 3 e 5 
