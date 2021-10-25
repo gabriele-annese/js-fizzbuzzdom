a31fd1fb1c5d6ee3928dd2404da63656732ed35f
gabriele-annese <gannese06@gmail.com>
2021-10-25 13:57:40 +0200

    add ml3 - ml5

--- js/main.js
+++ js/main.js
@@ -5,10 +5,22 @@ const container = document.querySelector(".container");
 // gen 10 row 
 
 for (let square = 1; square <= 100; square++) {
-let rowMarkup = '<div class="row">';
-rowMarkup += `<div class="square">${square}</div>`;
-rowMarkup += '</div>';
-container.innerHTML += rowMarkup;
+    let rowMarkup = '<div class="row">';
+    if( square % 3 ==  0){
+        rowMarkup += `<div class="square ml3">fizz</div>`;
+        rowMarkup += '</div>';
+        container.innerHTML += rowMarkup;
+    } else if (square % 5 == 0){
+        rowMarkup += `<div class="square ml5">buzz</div>`;
+        rowMarkup += '</div>';
+        container.innerHTML += rowMarkup;
+    } else{
+        let rowMarkup = '<div class="row">';
+        rowMarkup += `<div class="square">${square}</div>`;
+        rowMarkup += '</div>';
+        container.innerHTML += rowMarkup;
+    }
+
 }
 
 // individuare multipli di 3 
